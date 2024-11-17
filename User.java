2353b27f9e26a7b195b8f5c441397dabd916316b
trianglu <118457340+trianglu@users.noreply.github.com>
2024-11-17 16:45:15 -0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,44 @@
+//import javax.management.Notification;
+
+public class User {
+    private int userID;
+    private String name;
+    private String phoneNumber;
+    private String password;
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String recieveNotification(){
+        return phoneNumber;
+    } 
+}
