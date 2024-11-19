949aeadb0cf2d14d27a2babdbf8d739448a410a1
trianglu <118457340+trianglu@users.noreply.github.com>
2024-11-19 12:21:28 -0500

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,1 +1,17 @@
+import java.util.ArrayList;
 
+public class Bank{
+    private int bankID;
+    private String bankName;
+    private int accountNumber;
+    private double balance;
+
+    public void addBankAccount(){}
+
+    public void removeBankAccount(){}
+    
+    public void updateBalance(){}
+
+    public ArrayList<Double> getTransactions(double balance) {
+       
+    }
