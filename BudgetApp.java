f44f65f23f7e36d240aca10bc637599f55118d53
trianglu <118457340+trianglu@users.noreply.github.com>
2024-11-19 12:22:49 -0500

    Update BudgetApp.java

--- BudgetApp.java
+++ BudgetApp.java
@@ -1,10 +1,39 @@
 public class BudgetApp {
-    public static void main(String[] args) {
-        Account account = new Account();
+    public static Account account;
 
-        account.deposit(1000);
-        account.withdraw(500);
-        account.setProfile("Savings");
+    public static void createAccount() {
+        account = new Account();
+    }
+
+    public static Account getAccount() {
+        return account;
+    }
+
+    public static void setAccount(Account newAccount) {
+        account = newAccount;
+    }
+
+    public void deposit(double amount) {
+        account.deposit(amount);
+    }
+
+    public void withdraw(double amount) {
+        account.withdraw(amount);
+    }
+
+    public double getBalance() {
+        return account.getBalance();
+    }
+
+    public String getProfile() {
+        return account.getProfile();
+    }
+
+    public void setProfile(String newProfile) {
+        account.setProfile(newProfile);
+    }
+
+    public static void main(String[] args) {
         System.out.println("Balance: " + account.getBalance());
         System.out.println("Profile: " + account.getProfile());
     }
