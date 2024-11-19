6f25cf15c956380de20b151d99ea01b406d07d34
trianglu <118457340+trianglu@users.noreply.github.com>
2024-11-19 12:21:54 -0500

    Update Budget.java

--- Budget.java
+++ Budget.java
@@ -1,1 +1,15 @@
+public class Budget {
 
+    private double totalBudget;
+    private String catagory;
+    
+    public void setTotalBudget(double totalBudget){}
+
+    public void addcatogory(String catagory){}
+
+    public void removeCatogory(String catagory){}
+
+    public double checkRemaning(){
+        
+    }
+}
