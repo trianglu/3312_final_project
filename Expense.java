8497208dd5a7dfcf6944157d62f3a63dcd3c00ee
trianglu <arnavg@gmail.com>
2024-11-20 10:45:19 -0500

    update files

--- Expense.java
+++ Expense.java
@@ -1,1 +1,56 @@
+public class Expense {
 
+    private int expenceID;
+    private double amount;
+    private String date;
+    private String description;
+
+    public Expense(int expenceID, double amount, String date, String description) {
+        this.expenceID = expenceID;
+        this.amount = amount;
+        this.date = date;
+        this.description = description;
+    }
+
+
+    public String getDetails() {
+        return "Expense ID: " + expenceID + "\n" +
+               "Amount: $" + amount + "\n" +
+               "Date: " + date + "\n" +
+               "Description: " + description;
+    }
+
+  
+    public int getExpenceID() {
+        return expenceID;
+    }
+
+    public void setExpenceID(int expenceID) {
+        this.expenceID = expenceID;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
