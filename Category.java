88a1cee7f8f7ebc6ec8c86cfc85cfccfb14ef899
trianglu <118457340+trianglu@users.noreply.github.com>
2024-11-20 11:06:42 -0500

    Update Category.java

--- Category.java
+++ Category.java
@@ -1,7 +1,41 @@
 public class Category {
 
-    private int catagoryID;
+    private int categoryID;
     private String categoryName;
     private double allocatedAmount;
-    
+
+  
+    public Category(int categoryID, String categoryName, double allocatedAmount) {
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+        this.allocatedAmount = allocatedAmount;
+    }
+   
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+
+    public double getAllocatedAmount() {
+        return allocatedAmount;
+    }
+
+    public void setAllocatedAmount(double allocatedAmount) {
+        this.allocatedAmount = allocatedAmount;
+    }
+
+
 }
